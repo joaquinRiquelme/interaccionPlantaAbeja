9b2bb11283b25024480e4826378effed3a24404d
joaquinRiquelme <43077799+joaquinRiquelme@users.noreply.github.com>
2025-12-12 16:25:07 -0300

    plantas_wider

--- 01procesos/plantas.R
+++ 01procesos/plantas.R
@@ -16,52 +16,57 @@ head(plantas)
 plantas <- plantas[order(plantas$ID),]
 write.csv(plantas, file = "../02salidas/plantas.csv", row.names = FALSE)
 
+head(plantas)
+plantas$value <- 1
 
-especies <- base_datos$especies
-head(especies)
-especies <- subset(especies, orden=="Hymenoptera" & is.element(familia, 
-                                                               c("Andrenidae",
-                                                                 "Halictidae",
-                                                                 "Apidae",
-                                                                 "Megachilidae",
-                                                                 "Melittidae",
-                                                                 "Colletidae",
-                                                                 "Strenitidae")))
-table(especies$familia)
-tabla.e <- data.frame(table(especies$familia))
-names(tabla.e)[1] <- "Familia"
-tabla.f <- tabla.e[order(tabla.e$Freq, decreasing = TRUE),]
-# otras <- sum(tabla.f$Freq[11:nrow(tabla.f)])
-# tabla.f <- tabla.f[1:10,]
-# tabla.f <- rbind(tabla.f, data.frame(Var1="Otras", 
-                                     # Freq=otras))
+plantas_wider <- pivot_wider(plantas, names_from =  "ID", values_from = "value", values_fill = 0)
+write.csv(plantas_wider, file = "../02salidas/plantas_wider.csv", row.names = FALSE)
 
-tabla.f <- tabla.f[order(tabla.f$Familia, decreasing = FALSE),]
-
-
-tabla.f$porcentaje <- round(tabla.f$Freq / sum(tabla.f$Freq) *100, 1) 
-tabla.f$label_pos <- sum(tabla.f$Freq) - cumsum(tabla.f$Freq)+tabla.f$Freq/2
-
-
-
-p <- ggplot(tabla.f, aes(x='',y=Freq, fill= Familia))+
-  geom_bar(stat="identity", width=1)+
-  scale_fill_viridis_d()
-
-p
-
-pie <- p + coord_polar("y",start = -1)+
-  theme_void()+
-  labs(title='Número de especies de abeja por familia')+
-  geom_text(aes(y=label_pos, label=paste0(Freq, "\n", porcentaje, "%" )),
-            color="black", size=4)
-
-pie
-png("pieFamilias.png", width = 1280, height = 720, units ="px")
-pie
-dev.off()
-
-png("pieFamilias800.png", width = 800, height = 600, units ="px")
-pie
-dev.off()
+# especies <- base_datos$especies
+# head(especies)
+# especies <- subset(especies, orden=="Hymenoptera" & is.element(familia, 
+#                                                                c("Andrenidae",
+#                                                                  "Halictidae",
+#                                                                  "Apidae",
+#                                                                  "Megachilidae",
+#                                                                  "Melittidae",
+#                                                                  "Colletidae",
+#                                                                  "Strenitidae")))
+# table(especies$familia)
+# tabla.e <- data.frame(table(especies$familia))
+# names(tabla.e)[1] <- "Familia"
+# tabla.f <- tabla.e[order(tabla.e$Freq, decreasing = TRUE),]
+# # otras <- sum(tabla.f$Freq[11:nrow(tabla.f)])
+# # tabla.f <- tabla.f[1:10,]
+# # tabla.f <- rbind(tabla.f, data.frame(Var1="Otras", 
+#                                      # Freq=otras))
+# 
+# tabla.f <- tabla.f[order(tabla.f$Familia, decreasing = FALSE),]
+# 
+# 
+# tabla.f$porcentaje <- round(tabla.f$Freq / sum(tabla.f$Freq) *100, 1) 
+# tabla.f$label_pos <- sum(tabla.f$Freq) - cumsum(tabla.f$Freq)+tabla.f$Freq/2
+# 
+# 
+# 
+# p <- ggplot(tabla.f, aes(x='',y=Freq, fill= Familia))+
+#   geom_bar(stat="identity", width=1)+
+#   scale_fill_viridis_d()
+# 
+# p
+# 
+# pie <- p + coord_polar("y",start = -1)+
+#   theme_void()+
+#   labs(title='Número de especies de abeja por familia')+
+#   geom_text(aes(y=label_pos, label=paste0(Freq, "\n", porcentaje, "%" )),
+#             color="black", size=4)
+# 
+# pie
+# png("pieFamilias.png", width = 1280, height = 720, units ="px")
+# pie
+# dev.off()
+# 
+# png("pieFamilias800.png", width = 800, height = 600, units ="px")
+# pie
+# dev.off()
 
